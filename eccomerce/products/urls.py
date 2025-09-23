8abed3b6b96db261fc0ecc31ecdeeb3ace84da72
virginianjoki <njokivirginia0619@gmail.com>
2025-09-23 15:32:09 +0300

    capstone

---
+++ eccomerce/products/urls.py
@@ -0,0 +1,7 @@
+# products/urls.py
+from django.urls import path
+from .views import ProductListView
+
+urlpatterns = [
+    path('products/', ProductListView.as_view(), name='product-list'),
+]
---
+++ eccomerce/users/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["email"] = user.email
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+

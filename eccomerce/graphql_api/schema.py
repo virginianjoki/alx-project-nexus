7c5f417902d87efb77e3d4689e61dc9e4357a1f4
virginianjoki <njokivirginia0619@gmail.com>
2025-09-27 20:20:28 +0300

    eccomerce

---
+++ eccomerce/graphql_api/schema.py
@@ -0,0 +1,29 @@
+import graphene
+from graphene_django import DjangoObjectType
+from products.models import Product, Category
+
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class Query(graphene.ObjectType):
+    all_products = graphene.List(ProductType)
+    all_categories = graphene.List(CategoryType)
+
+    def resolve_all_products(root, info):
+        return Product.objects.all()
+
+    def resolve_all_categories(root, info):
+        return Category.objects.all()
+
+
+schema = graphene.Schema(query=Query)
---
+++ eccomerce/products/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+# products/views.py
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['category', 'brand', 'availability']
+    ordering_fields = ['price', 'created_at']
+    ordering = ['created_at']  # default ordering
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer

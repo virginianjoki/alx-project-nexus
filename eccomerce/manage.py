0376477d54ccfc4e46288640766c4598f108be71
virginianjoki <njokivirginia0619@gmail.com>
2025-09-27 19:44:23 +0300

    products

--- eccomerce/manage.py
+++ eccomerce/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eccomerce.eccomerce.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eccomerce.settings")
 
 
 def main():

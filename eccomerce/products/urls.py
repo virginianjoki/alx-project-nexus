7a4acd885151cd0cd0beadb75ef89bf053831a80
virginianjoki <njokivirginia0619@gmail.com>
2025-09-27 15:24:11 +0300

    delete

--- eccomerce/products/urls.py
+++
@@ -1,7 +0,0 @@
-# products/urls.py
-from django.urls import path
-from .views import ProductListView
-
-urlpatterns = [
-    path('products/', ProductListView.as_view(), name='product-list'),
-]
--- eccomerce/users/tasks.py
+++
@@ -1,8 +0,0 @@
-import time
-from celery import shared_task
-
-
-@shared_task
-def send_welcome_email(email):
-    time.sleep(5)  # simulate long task
-    return f"Email sent to {email}"

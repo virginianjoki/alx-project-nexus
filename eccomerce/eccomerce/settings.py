69c7e9bbc8d40064315267cccd5fb35111cfffa0
virginianjoki <njokivirginia0619@gmail.com>
2025-09-28 11:15:49 +0300

    gunicorn

--- eccomerce/eccomerce/settings.py
+++ eccomerce/eccomerce/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
